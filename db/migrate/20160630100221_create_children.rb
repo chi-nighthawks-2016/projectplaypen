94f0db79be16b14197fa2d7527025d1bf7437ab1
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-06-30 10:34:30 -0500

    create migrations

--- db/migrate/20160630100221_create_children.rb
+++ db/migrate/20160630100221_create_children.rb
@@ -1,4 +1,12 @@
 class CreateChildren < ActiveRecord::Migration
   def change
+    create_table :children do |t|
+      t.integer :parent_id, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.date :birthday, null: false
+      t.string :allergies, null: false
+      t.timestamps(null: false)
+    end
   end
 end
--- db/migrate/20160630100250_create_playdates.rb
+++ db/migrate/20160630100250_create_playdates.rb
@@ -1,4 +1,11 @@
 class CreatePlaydates < ActiveRecord::Migration
   def change
+    t.integer :host_id, null: false
+    t.time :time, null: false
+    t.date :date, null: false
+    t.integer :duration, null: false
+    t.string :location, null: false
+    t.string :description, null: false
+    t.timestamps(null: false)
   end
 end
