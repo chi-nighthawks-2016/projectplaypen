c777c1f2f791fb565dfaade47338289825ba48a2
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-06-30 14:07:50 -0500

    seed data and associations okay, I think

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,19 @@
+users = []
 
 1.times {
-   User.create!(
+   users << User.create!(
     username: "amanda",
-    pw_hash: "password",
+    password: "password",
     email: "adnama.lin@gmail.com",
     first_name: "amanda",
     last_name: "lin",
     phone: "3309620922")
 }
 
-users = []
-
 10.times {
    users << User.create!(
     username: Faker::Internet.user_name,
-    pw_hash: Faker::Internet.password(8),
+    password: Faker::Internet.password(8),
     email: Faker::Internet.free_email,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -22,16 +21,17 @@ users = []
 }
 
 10.times {
-   users.sample.playdates.build(
+   users.sample.playdates.create(
     time: Faker::Time.forward(23, :morning).to_s.match(/\d{2}:\d{2}:\d{2}/).to_s,
     date:Faker::Time.forward(23),
     duration:Faker::Number.between(1, 4),
     location:"#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state_abbr}",
     description: Faker::Hipster.paragraph,
     title: Faker::Hipster.sentence)
+}
 
-12.times {
-  users.sample.children.build(
+20.times {
+  users.sample.children.create(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   birthday: Faker::Date.between(4.year.ago, Date.today),
@@ -39,8 +39,7 @@ users = []
 }
 
 10.times {
-  user = users.sample
-  user.rsvps.build(
-    child_id: user.children.sample
+  Rsvp.create(
+    child_id: Faker::Number.between(1, 12),
     playdate_id: Faker::Number.between(1, 10))
 }
