81ab4a3f33a1918bf80088025455220631e1e3fa
Vivien Kuo <email": null>
2016-06-30 10:52:30 -0500

    finish migration, models, association, and seed data

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,8 @@
 class Child < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :parent, class_name: "User"
+  has_many :rsvps
+
+  validates :parent_id, presence: true
+  validates :first_name, presence: true
+  validates :birthday, presence: true
 end
--- db/migrate/20160630100221_create_children.rb
+++ db/migrate/20160630100221_create_children.rb
@@ -1,4 +1,12 @@
 class CreateChildren < ActiveRecord::Migration
   def change
+    create_table :children do |t|
+      t.integer :parent_id, null: false
+      t.string :first_name, null:false
+      t.string :last_name
+      t.datetime :birthday, null:false
+      t.string :allergies
+      t.timestamps(null: false)
+    end
   end
 end
