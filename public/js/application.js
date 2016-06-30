7adc7433dc3cead3375b73c0f5bcf7125cba9002
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-06-30 16:55:08 -0500

    got a calendar up

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-
+  $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+    
   $("a[class='playdate-title']").on("click", function(e){
     e.preventDefault();
     var route = $(this).attr("href");
