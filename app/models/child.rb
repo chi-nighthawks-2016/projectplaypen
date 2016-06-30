432762a3d8c2c34a5aea25bec6b7e574687392f8
Vivien Kuo <email": null>
2016-06-30 11:10:53 -0500

    create age virtual attribute

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,4 +5,8 @@ class Child < ActiveRecord::Base
   validates :parent_id, presence: true
   validates :first_name, presence: true
   validates :birthday, presence: true
+
+  def age
+    ((Time.now - birthday)/(24 * 60 * 60 * 7 * 52)).floor
+  end
 end
