a6e9c7e42e8c04395d7dc3434b1330deb7178fde
Amanda Lin & Benjamin Shyman & Vivien Kuo <pair+adnamalin.bshyman.viKuo@devbootcamp.com>
2016-06-30 14:11:31 -0500

    Working on user-css

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -15,3 +15,15 @@ end
 get '/register' do
   erb :'/user/user_new'
 end
+
+post '/user' do
+  @user = User.new(username: params[:username], email: params[:email], password: params[:password], first_name: params[:first_name], last_name: params[:last_name], phone: params[:phone] )
+  p @user
+  if @user.save
+    @message = "You have been successfuly registered!"
+    erb :"/user/#{@user.id}/playdates"
+  else
+    @errors = @user.errors
+    erb :'user/user_new'
+  end
+end
