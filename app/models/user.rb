42455cfa695043a0c9c23361ed8736e5c51a9280
Amanda Lin & Benjamin Shyman & Vivien Kuo <pair+adnamalin.bshyman.viKuo@devbootcamp.com>
2016-06-30 11:14:39 -0500

    Semi user story

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :children
+  has_many :rsvps, through: :children
+  has_many :playdates, foregin_key: :host_id
+  has_many :hostings, class_name: :Playdate, foreign_key: :host_id
+
+
+  validates :username, presence: { message: "cannot be empty" },
+                      uniqueness: true
+  validates :email, presence:  { message: "cannot be empty" },
+                      uniqueness: true
+  validates :pw_hash, presence: { message: "cannot be empty" }
+
+  def password
+    @password ||= BCrypt::Password.new(pw_hash)
+  end
+
+  def password=(new_password)
+    if !new_password.empty?
+      @password = BCrypt::Password.create(new_password)
+      self.pw_hash = @password
+    end
+  end
+
+  def authenticate(typed_password)
+    self.password == typed_password
+  end
 end
