a05836702d22ed4c8e609d467c651b2e914a2fbc
Vivien Kuo <lost.angel.fin@gmail.com>
2016-06-30 20:32:40 -0500

    made routes for edit playdates

--- app/controllers/playdate.rb
+++ app/controllers/playdate.rb
@@ -1,9 +1,9 @@
-get '/hostings/new' do
+get '/playdates/new' do
  log_in unless current_user
  erb :'/playdates/new'
 end
 
-post '/hostings' do
+post '/playdates' do
   new_playdate = current_user.hostings.new(params[:entry])
   if new_playdate.save
     redirect "/playdates"
@@ -16,4 +16,14 @@ end
 get '/playdates/:id' do
   @playdate = Playdate.find(params[:id])
   erb :'playdates/_playdate_details', locals: {playdate: @playdate}, layout: false
+end
+
+get '/playdates/:id/edit' do
+	edit_playdate = Playdate.find(params[:id])
+	edit_playdate.assign_attributes(params[:entry])
+	if edit_playdate.save
+		edit_playdate
+	else
+		edit_playdate.errors.full_messages.join(". ")
+	end
 end
