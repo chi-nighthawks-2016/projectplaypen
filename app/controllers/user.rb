1736818213847248251a1e450c805e229b17c9f3
Amanda Lin & Benjamin Shyman & Vivien Kuo <pair+adnamalin.bshyman.viKuo@devbootcamp.com>
2016-07-01 13:55:09 -0500

    Added location links and css

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -6,7 +6,7 @@ post '/login' do
   @user = User.find_by(username: params[:username])
   if @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
-    redirect ("/users/#{@user.id}")
+    redirect "/users/#{@user.id}"
   else
     @errors = "Invalid Username or Password"
     erb :'users/login'
