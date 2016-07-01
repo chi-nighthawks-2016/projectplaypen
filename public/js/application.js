96d56d56a17c4fb9ef6964e3e914b40848768b90
Vivien Kuo <lost.angel.fin@gmail.com>
2016-06-30 20:55:25 -0500

    implemented attendee # and attendee list. needs to be run

--- public/js/application.js
+++ public/js/application.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
 
   $("a#playdate-title").on("click", function(e){
     e.preventDefault();
-    var route = $(this).attr("href");
+    var route = $(this).attr("href"); //what are you doing with the route??
     var $thisPlaydate = $(this).parent();
     var thisPlaydateDetails = $thisPlaydate.siblings(".playdate-details")
     $(thisPlaydateDetails).slideToggle("slow");
+  });
+
+   $("span#attendee-title").on("click", function(e){
+    e.preventDefault();
+    $("span#attendee-detail").slideToggle("slow");
   });
 });
